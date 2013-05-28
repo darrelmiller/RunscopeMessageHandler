a638f2e5a854a4b25da16109c402083e1656de03
Darrel <darrel@tavis.ca>
2013-05-28 16:00:00 -0400

    First commit

---
+++ RunscopeMessageHandler.Tests/TestHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using System.Linq;
+
+namespace Runscope.Contrib.Tests
+{
+    public class TestHandler
+    {
+        [Theory,
+        InlineData("foo", "http://example.org/", "http://example-org-foo.runscope.net/"),
+        InlineData("foo", "http://example.org/with/a/path", "http://example-org-foo.runscope.net/with/a/path"),
+        InlineData("foo", "http://example.org/with/a/path?and=query&string=true", "http://example-org-foo.runscope.net/with/a/path?and=query&string=true"),
+        InlineData("foo", "http://example.org/with/a/path#andFragment", "http://example-org-foo.runscope.net/with/a/path#andFragment"),
+        InlineData("bar", "http://example.org:99/", "http://example-org-bar.runscope.net/"),
+        InlineData("bar", "https://example.org/", "https://example-org-bar.runscope.net/"),
+        InlineData("bar", "https://example.org:871/", "https://example-org-bar.runscope.net/"),
+        InlineData("pass", "https://jane:doe@example.org:871/", "https://jane:doe@example-org-pass.runscope.net/"),
+        InlineData("path", "https://foo-bar.example.org/~home", "https://foo--bar-example-org-path.runscope.net/~home"),
+        ]
+        public Task CreateRequest(string bucket, string input, string expected)
+        {
+            var invoker = new HttpMessageInvoker(new RunscopeMessageHandler(bucket,new FakeHandler()));
+
+            var inputUri = new Uri(input);
+
+            var request = new HttpRequestMessage() {RequestUri = inputUri};
+            return invoker.SendAsync(request, new CancellationToken()).ContinueWith(t =>
+                {
+                    var response = t.Result;
+                    Assert.Equal(expected, response.RequestMessage.RequestUri.OriginalString);
+
+                    if ((inputUri.Scheme == "http" && inputUri.Port != 80) ||
+                        (inputUri.Scheme == "https" && inputUri.Port != 443))
+                    {
+                        var header = response.RequestMessage.Headers.GetValues("Runscope-Request-Port").FirstOrDefault();
+                        Assert.NotNull(header);
+                        Assert.Equal(header, inputUri.Port.ToString());
+                    } 
+                });
+
+            
+        }
+
+        
+    }
+
+    public class FakeHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<HttpResponseMessage>();
+            tcs.SetResult(new HttpResponseMessage() {RequestMessage =  request});
+            return tcs.Task;
+        }
+    }
+
+
+
+}
---
+++ RunscopeMessageHandler/RunscopeMessageHandler/RunscopeMessageHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Runscope.Contrib
+{
+    public class RunscopeMessageHandler : DelegatingHandler
+    {
+        private readonly string _bucketKey;
+        
+        public RunscopeMessageHandler(string bucketKey, HttpMessageHandler innerHandler)
+        {
+            _bucketKey = bucketKey;
+            InnerHandler = innerHandler;
+        }
+
+        protected override System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            var requestUri = request.RequestUri;
+            var port = requestUri.Port;
+
+            request.RequestUri = ProxifyUri(requestUri, _bucketKey);
+            if ((requestUri.Scheme == "http" && port != 80 )||  requestUri.Scheme == "https" && port != 443)
+            {
+                request.Headers.TryAddWithoutValidation("Runscope-Request-Port", port.ToString());
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        private Uri ProxifyUri(Uri requestUri, string bucketKey, string gatewayHost = "runscope.net")
+        {
+            var cleanHost = requestUri.Host.Replace("-","~").Replace(".","-");
+            var newHost = String.Format("{0}-{1}.{2}", cleanHost, bucketKey, gatewayHost).Replace("~","--");
+
+            string userName = null;
+            string password = null;
+            if (!String.IsNullOrEmpty(requestUri.UserInfo))
+            {
+               var info = requestUri.UserInfo.Split(':');
+                if (info.Length == 2)
+                {
+                    password = info[1];
+                }
+                userName = info[0];
+            }
+
+            var uriBuilder = new UriBuilder()
+                {
+                    Scheme = requestUri.Scheme,
+                    Host = newHost,
+                    UserName = userName,
+                    Password = password,
+                    Port = -1,
+                    Path = requestUri.AbsolutePath,
+                    Query = requestUri.Query.StartsWith("?") ?  requestUri.Query.Substring(1) : requestUri.Query,  // Remove leading ?
+                    Fragment = requestUri.Fragment.StartsWith("#") ? requestUri.Fragment.Substring(1) : requestUri.Fragment
+                };
+            return uriBuilder.Uri;
+        }
+    }
+}
